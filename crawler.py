a0293255cbeb58f2496d6dd34fdbb88bb0a98c78
Jose Miguel <jmn8718@gmail.com>
2016-03-29 23:57:05 +0200

    added code and exporting data

--- crawler.py
+++ crawler.py
@@ -1,39 +1,81 @@
 import urllib
 from bs4 import BeautifulSoup
+import json
+from time import localtime, strftime
 
-def print_links(html):
+NOT_FOUND = 'NotFound'
+
+def to_file(crawled):
+	timestamp = strftime("%Y%m%d%H%M%S", localtime())
+	print timestamp
+	#print json.dumps(crawled)
+	filename = timestamp + '_crawl.json'
+	with open(filename, 'w') as outfile:
+		json.dump(crawled, outfile, indent=2)
+
+def get_all_links(html):
 	links = []
 	for link in html.find_all('a'):
 		links.append(link.get('href'))
-    	#print(link.get('href'))
-	print len(links)
+	return links
+
+def print_links(html):
+	print len(get_all_links(html))
 
 
-def print_unique_links(html):
+def get_unique_links(html):
 	links = []
 	for link in html.find_all('a'):
 		href = link.get('href')
-		if href not in links:
+		if href not in links and href != None:
 			links.append(href)
-	    	#print(link.get('href'))
-	print len(links)
+	return links
 
+def print_unique_links(html):
+	print len(get_unique_links(html))
 
+def get_url_content(url):
+	try:
+		page = urllib.urlopen(url).read()
+		return page
+	except:
+		return NOT_FOUND
 
+def evaluate_content(content):
+	result = ['error404']
 
-def crawl_web(url):
-	print 'Crawling '+url
-	page = urllib.urlopen(url).read()
+	soup = BeautifulSoup(content, 'html.parser')
 
-	soup = BeautifulSoup(page, 'html.parser')
+	if len(soup.find_all("section", class_="error404")) == 0:
+		result = get_unique_links(soup)
 
-	if len(soup.find_all("section", class_="error404")) == 0 :
-		print 'Page with content'
-		print_links(soup)
-		print_unique_links(soup)
-	else:
-		print 'ERROR 404'
+	return result
+
+def crawl_web(seed):
+	print 'Crawling '+seed
+	
+	to_crawl = [seed]
+	crawled = {}
+
+	while len(to_crawl)>0:
+		url = to_crawl.pop()
+		if url not in crawled:
+			content = get_url_content(url)
+			if content == NOT_FOUND:
+				links = [content]
+			else:
+				links = evaluate_content(content)
+			crawled[url]=links
+		else:
+			print 'URL: '+url+ ' || Already done'
+
+	to_file(crawled)
 	print 'Done crawling'
 
-crawl_web("http://www.bbvaapimarket.com/")
-crawl_web('https://www.bbvaapimarket.com/web/api_market/ffa')
+
+def main():
+	crawl_web("http://www.bbvaapimarket.com/")
+	#crawl_web('https://www.bbvaapimarket.com/web/api_market/ffa')
+
+if __name__ == "__main__":
+    main()
