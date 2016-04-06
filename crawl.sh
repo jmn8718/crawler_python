2a8cddc6944cd8fe080add135df9a76320658cca
Jose Miguel <josemiguel.navarro@bbva.com>
2016-04-06 09:47:22 +0200

    updated sh

--- crawl.sh
+++ crawl.sh
@@ -9,20 +9,11 @@ fi
 #python crawler.py http://www.bbvaapimarket.com/
 python main.py PRO
 
-FILE=$(ls -r data | head -1)
+#FILE=$(ls -r data | head -1)
 
-if [ -f "data/$FILE" ]; then
- 	echo 'file '$FILE' exist'
+#if [ -f "data/$FILE" ]; then
+# 	echo 'file '$FILE' exist'
  	#python template.py $FILE
-else
-	echo 'No file to process'
-fi
-
-#PROJECT_ID=dev-center/raml
-#TOKEN=iZ8f2RZkT5zLhEFaE1BA
-#FILE_ID=s
-#curl https://gitlab.digitalservices.es/api/v3/projects/333/repository/tree?private_token=$TOKEN -k
-
-#curl -k https://gitlab.digitalservices.es/dev-center/raml/raw/develop/US_Security.raml?private_token=$TOKEN
-
-#https://gitlab.digitalservices.es/dev-center/raml/raw/develop/US_Security.raml?private_token=iZ8f2RZkT5zLhEFaE1BA
+#else
+#	echo 'No file to process'
+#fi
